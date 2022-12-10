3029fde74583459b4163d75831d86c347d66f362
Gloriousjoy <gloriousfab99@gmail.com>
2022-12-10 19:38:40 +0300

    login form

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Routes, Route} from "react-router-dom";
+import About from "./components/About"
+
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 <BrowserRouter>
+<Routes>
+            <Route path="/" element={<About />} />
+            </Routes>
     <App />
 </BrowserRouter>,
 document.getElementById('root')
